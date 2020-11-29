dcaa969a74091437a63b3a236e9bc9fa7a4d322b
maikeeb <leaguehomey@gmail.com>
2020-11-29 18:06:55 -0500

    updated engine for pause menu

--- GameEngine_HK/Engine.cpp
+++ GameEngine_HK/Engine.cpp
@@ -10,15 +10,31 @@ void Engine::Update()
 	sf::Event _event;
 	while (window -> pollEvent(_event))
 	{
+	
 		//"close requested" event: we close the window
 		if (_event.type == sf::Event::Closed)
 		{
 			std::cout << "Closing now..." << std::endl;
 			window -> close();
 		}
-	}
 
+		pauseMenu.Update(_event, 10.0f, this->window);
+
+	}
+	
 	world->tick(10.0f); // milliseconds inbetween ticks
+
+	mainCam.Update(world, 10.0f, window);
+
+	if (States::GetPausedState() == true)
+	{
+		GameInactiveStaticActions();
+	}
+}
+
+void Engine::GameInactiveStaticActions()
+{
+	pauseMenu.Render(this->window, 10.0f, mainCam.cameraView.getCenter());
 }
 
 Engine& Engine::GetInstance()
@@ -32,6 +48,11 @@ void Engine::Start(sf::RenderWindow* win)
 {
 	bIsQuit = false;
 	window = win;
+
+	pauseMenu = PauseMenu(this->window);
+	mainCam = MainCamera(sf::Vector2f(
+		this->window->getSize().x / 2, 
+		this->window->getSize().y / 2));
 
 	// run the program as long as the window is open
 	while (window -> isOpen())
--- GameEngine_HK/include/Engine.h
+++ GameEngine_HK/include/Engine.h
@@ -1,10 +1,21 @@
 #pragma once
 
 #include <iostream>
+
 #include <SFML/Graphics.hpp>
 #include "ECS.h"
+
 #include "RenderingSystem.h"
 #include "AnimationSystem.h"
+#include "InputSystem.h"
+#include "MovementSystem.h"
+#include "PhysicsSystem.h"
+#include "TileMapSystem.h"
+
+#include "MainCamera.h"	
+#include "PauseMenu.h"
+#include "Components.h"
+
 
 class Engine
 {
@@ -17,13 +28,16 @@ private:
 	Engine(Engine& copy);		// hide copy constructor
 	Engine(Engine&& other);		// hide move constructor
 	Engine& operator= (Engine& copy); //hide assignment operator
+
 	~Engine();
 	void Update();
+	void GameInactiveStaticActions();
 
 public:
 	sf::RenderWindow* window;
 	ECS::World* world;
-
+	MainCamera mainCam;
+	PauseMenu pauseMenu;
 
 	static Engine& GetInstance();
 	void Start(sf::RenderWindow* win);
